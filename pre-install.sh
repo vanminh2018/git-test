3aeb60b71cb494fb7920c68209484df420fe5dcb
vanminh2018 <45564619+vanminh2018@users.noreply.github.com>
2024-10-28 14:17:24 +0700

    Update swappiness

--- pre-install.sh
+++ pre-install.sh
@@ -47,7 +47,7 @@ net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
 net.ipv4.ip_forward = 1
-vm.swappiness = 1
+vm.swappiness = 10
 EOF
 
 sysctl -p /etc/sysctl.conf
